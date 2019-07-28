336b4b64cbf9ba55ca552ad0a9e460618013fdcb
HexaCubist <me@hexacubist.com>
2019-07-28 21:41:27 +1200

    Configure dropzone and add form elements

--- public/js/main.js
+++ public/js/main.js
@@ -3,12 +3,25 @@
 $(document).ready(() => {
 
   // Create dropzone form
-  $('form#upload-zone').dropzone({
+  $('#upload-zone').dropzone({
     url: '/upload',
     maxFilesize: 2, // MB
     acceptedFiles: '.csv,.xls,.xlsx',
     autoProcessQueue: false,
     maxFiles: 1,
+    previewTemplate: `
+    <div class='dz-details row text-left'>
+      <div class="col-6 dz-detail dz-name" data-dz-name></div>
+      <div class="col-4 dz-detail dz-size" data-dz-size></div>
+      <div class="col-2"><a class="btn btn-danger btn-large btn-link dz-remove" href="javascript:undefined;" data-dz-remove="">Delete</a></div>
+      </div>
+    <div class="dz-error-message"><span data-dz-errormessage></span></div>
+    <div class="dz-success-mark"><span>✔</span></div>
+    <div class="dz-error-mark"><span>✘</span></div>
+    <div class="float-left dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>
+    <div class="float-left"></div>
+    <div class="float-left"></div>
+    `,
     init() {
       const dz = this;
       // Source: https://stackoverflow.com/a/35275260
@@ -29,7 +42,28 @@ $(document).ready(() => {
         this.removeAllFiles();
         this.addFile(file);
       });
+
+      // Show next step when files added
+      this.on('maxfilesreached', () => { $('#step2').animate({opacity: 1, height: 'toggle' }, 250); });
+      this.on('removedfile', () => { $('#step2').animate({ opacity: 0, height: 'toggle' }, 250); });
     }
   });
 
+  // Date Time Picker from https://demos.creative-tim.com/material-kit/docs/2.1/plugins/datetimepicker.html
+  $('.datetimepicker').datetimepicker({
+    icons: {
+      time: 'mi mi-access-time',
+      date: 'mi mi-today',
+      up: 'mi mi-chevron-left mi-chevron-rotate',
+      down: 'mi mi-chevron-right mi-chevron-rotate',
+      previous: 'mi mi-chevron-left',
+      next: 'mi mi-chevron-right',
+      today: 'mi mi-today',
+      clear: 'mi mi-trash',
+      close: 'mi mi-remove'
+    }
+  });
+
+  // Hide step 2 using JavaScript (so that it remains visible without JS)
+  $('#step2').animate({ opacity: 1, height: 'toggle' }, 0);
 });
