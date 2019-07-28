4e16da473e274e1c5dce2c321f7882e560e9ac2a
HexaCubist <me@hexacubist.com>
2019-07-28 19:29:40 +1200

    Use Dropzone

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,35 @@
+/* eslint-disable func-names */
 /* eslint-env jquery, browser */
 $(document).ready(() => {
 
-  // Place JavaScript code here...
+  // Create dropzone form
+  $('form#upload-zone').dropzone({
+    url: '/upload',
+    maxFilesize: 2, // MB
+    acceptedFiles: '.csv,.xls,.xlsx',
+    autoProcessQueue: false,
+    maxFiles: 1,
+    init() {
+      const dz = this;
+      // Source: https://stackoverflow.com/a/35275260
+      // for Dropzone to process the queue (instead of default form behavior):
+      $('#submit-all').on('click', (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        dz.processQueue();
+      });
+      // send all the form data along with the files:
+      this.on('sendingmultiple', (data, xhr, formData) => {
+        formData.append('firstname', $('#firstname').val());
+        formData.append('lastname', $('#lastname').val());
+      });
+
+      // Source: https://stackoverflow.com/a/26035954/3902950
+      this.on('maxfilesexceeded', function (file) {
+        this.removeAllFiles();
+        this.addFile(file);
+      });
+    }
+  });
 
 });
