15ff39d9c980f649d37e06414232cb0f41f952a1
HexaCubist <me@hexacubist.com>
2019-08-18 19:35:11 +1200

    Finish setting up Swish Source

--- SwishSource/swish.js
+++ SwishSource/swish.js
@@ -17,14 +17,15 @@ const DefaultSettings = {
   LOCK_DATE: (new Date('2019/12/31 23:59:59')).toISOString(),
   TOTAL_MARKS_PER_QUIZ: 50,
   NUMBER_OF_QUESTIONS_PER_QUIZ: 5,
-  MARKS_PER_QUESTION: TOTAL_MARKS_PER_QUIZ / NUMBER_OF_QUESTIONS_PER_QUIZ
+  MARKS_PER_QUESTION: this.TOTAL_MARKS_PER_QUIZ / this.NUMBER_OF_QUESTIONS_PER_QUIZ
 };
 
 const SwishSource = class {
   constructor(options = {}) {
-    Object.assign(this, DefaultSettings, options);
-    this.options = options;
+    this.settings = {};
+    Object.assign(this.settings, DefaultSettings, options);
     this.quizIds = ['assignmentId|quizId|canvasStudentId'];
+    util.init(this.settings.TOKEN);
     this.client = new RestClient();
   }
 
@@ -37,31 +38,31 @@ const SwishSource = class {
      *    Add override
      */
     // Load student questions and answers
-    this.options.QA_FILE = path.join(this.options.resourcesPath, this.options.QA_FILE);
-    this.options.studentsQA = util.loadStudentQaFile(this.options.QA_FILE);
+    this.settings.QA_FILE = path.join(this.settings.resourcesPath, this.settings.QA_FILE);
+    this.settings.studentsQA = util.loadStudentQaFile(this.settings.QA_FILE);
 
     // Check and proceed only if:
     //  - every question has an answer and the answer is truthy or 0
     //  - every student has a non-blank description
-    const truthyCheckResult = util.checkTruthy(this.options.studentsQA)
+    const truthyCheckResult = util.checkTruthy(this.settings.studentsQA)
     if (truthyCheckResult.errors) {
       console.error(`\nErrors found in QA file:\n${truthyCheckResult.errors}`);
       process.exit();
     }
 
     // Get Canvas student ids
-    this.getCanvasUserIds(`${this.options.DOMAIN}/api/v1/courses/${this.options.COURSE_ID}/users?enrollment_type[]=student&per_page=100`)
+    this.getCanvasUserIds(`${this.settings.DOMAIN}/api/v1/courses/${this.settings.COURSE_ID}/users?enrollment_type[]=student&per_page=100`)
     // When getUsers finishes recursing it calls createQuizzes().
   }
 
   // When getUsers finishes recursing it calls createQuizzes().
-  createQuizzes(studentsQA) {
+  createQuizzes() {
     // Remove students who don't exist on Canvas:
     // Filter objects which have the auid property, then map to an array of those auid values.
     const auids = [];
-    Object.keys(studentsQA).forEach((k) => {
-      if (studentsQA[k].auid) {
-        auids.push(studentsQA[k].auid);
+    Object.keys(this.settings.studentsQA).forEach((k) => {
+      if (this.settings.studentsQA[k].auid) {
+        auids.push(this.settings.studentsQA[k].auid);
       } else {
         console.log(`skipped ${k} | in studentsQA file but not in Canvas`);
       }
@@ -72,7 +73,7 @@ const SwishSource = class {
     async.eachSeries(auids, (auid, studentDone) => {
       const title = `${this.settings.ASSIGNMENT_TITLE} for ${auid}`;
 
-      const { description } = studentsQA[auid];
+      const { description } = this.settings.studentsQA[auid];
 
       const quizArgs = util.newQuizArgs(title, description, this.settings.TOTAL_MARKS_PER_QUIZ, this.settings.NUMBER_OF_ATTEMPTS);
 
@@ -83,7 +84,7 @@ const SwishSource = class {
 
         const quizId = data.id;
         const assignmentId = data.assignment_id;
-        const canvasStudentId = studentsQA[auid].id;
+        const canvasStudentId = this.settings.studentsQA[auid].id;
 
         this.quizIds.push(`${assignmentId}|${quizId}|${canvasStudentId}`);
 
@@ -138,7 +139,7 @@ const SwishSource = class {
           // eslint-disable-next-line function-paren-newline
         );
         this.client.post(url, args, (data, response) => {
-          this.questionDone();
+          questionDone();
         });
       });
     }
@@ -179,3 +180,10 @@ const SwishSource = class {
 };
 
 module.exports = SwishSource;
+
+// Testing
+if (require.main === module) {
+  console.log('called directly.. Running test!');
+  const x = new SwishSource();
+  x.start();
+}
